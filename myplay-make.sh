d9e170e71dc30cdcd1739ad41b67f237a0a594b4
necto <necto.ne@gmail.com>
2011-12-10 15:03:36 +0400

    little fix in build script

--- myplay-make.sh
+++ myplay-make.sh
@@ -4,7 +4,7 @@ SYS_LIBS="-lm -ljack -lasound -lva -lz -lbz2"
 
 FFMPEG_LIBS="libavfilter/libavfilter.a  libavformat/libavformat.a libavcodec/libavcodec.a libswscale/libswscale.a libswresample/libswresample.a libavutil/libavutil.a"
 
-gcc -c mplay.c -g
+gcc -c mplay.c -g -I. -I.. -I./*
 #make ffplay
 make $FFMPEG_LIBS ADDITIONAL_FLAGS_GIVEN_BY_CMDLINE=-DSTEGANOGRAPHY_STREAM
 gcc  mplay.o -o myplay $SYS_LIBS $FFMPEG_LIBS 
