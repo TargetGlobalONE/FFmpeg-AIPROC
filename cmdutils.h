20d956f2f2861bc7fc9fac638949d38022ae91e5
Target Global ONE LTD <114310351+TargetGlobalONE@users.noreply.github.com>
2023-01-20 03:38:24 +0000

    Update cmdutils.h

--- cmdutils.h
+++ cmdutils.h
@@ -17,6 +17,9 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with FFmpeg; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *
+ * FOR TEST (MJPG PROCESS)
  */
 
 #ifndef FFMPEG_CMDUTILS_H
